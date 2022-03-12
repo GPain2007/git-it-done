5e1b4405daa7145b3508b072e58254719e7dbe6f
Govnor Payne <govnorpayne@gmail.com>
2022-03-12 07:47:16 -0600

    added issues selection

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,48 +4,55 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var formSubmitHandler = function (event) {
+  // prevent page from refreshing
   event.preventDefault();
+
   // get value from input element
   var username = nameInputEl.value.trim();
 
   if (username) {
     getUserRepos(username);
+
+    // clear old content
+    repoContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
     alert("Please enter a GitHub username");
   }
-  console.log(event);
 };
 
 var getUserRepos = function (user) {
-  // check if api returned any repos
-  if (repos.length === 0) {
-    repoContainerEl.textContent = "No repositories found.";
-    return;
-  }
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-  // make a request to the url
+  // make a get request to url
   fetch(apiUrl)
     .then(function (response) {
+      // request was successful
       if (response.ok) {
+        console.log(response);
         response.json().then(function (data) {
+          console.log(data);
           displayRepos(data, user);
         });
       } else {
-        alert("Error: GitHub User Not Found");
+        alert("Error: " + response.statusText);
       }
     })
     .catch(function (error) {
-      // Notice this `.catch()` getting chained onto the end of the `.then()` method
       alert("Unable to connect to GitHub");
     });
 };
 
 var displayRepos = function (repos, searchTerm) {
-  repoContainerEl.textContent = "";
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
+
   repoSearchTerm.textContent = searchTerm;
+
   // loop over repos
   for (var i = 0; i < repos.length; i++) {
     // format repo name
@@ -78,17 +85,12 @@ var displayRepos = function (repos, searchTerm) {
     }
 
     // append to container
+    repoEl.appendChild(statusEl);
 
+    // append container to the dom
     repoContainerEl.appendChild(repoEl);
   }
 };
 
-var response = fetch("https://api.github.com/users/octocat/repos").then(
-  function (response) {
-    response.json().then(function (data) {
-      console.log(data);
-    });
-  }
-);
-
+// add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
